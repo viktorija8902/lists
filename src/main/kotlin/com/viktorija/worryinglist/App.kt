a314195b2fbf67aa7f06afbf709ed938593a77a1
Viktorija <viktorija.bezsazna@gmail.com>
2019-08-17 22:22:11 -0400

    Write 'Hello world' in browser

--- src/main/kotlin/com/viktorija/worryinglist/App.kt
+++ src/main/kotlin/com/viktorija/worryinglist/App.kt
@@ -1,5 +1,22 @@
 package com.viktorija.worryinglist
 
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
 fun main() {
-    println("Hello world!")
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }
+    server.start(wait = true)
 }
